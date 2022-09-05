8dd07fe9cf265b391dbd6f8e538cfc9c7850e01d
NILUFAR KHAYDAROVA <38408269+NilufarKhaydarova@users.noreply.github.com>
2022-09-05 16:34:32 +0500

    2

---
+++ bot.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, Response
+import requests
+import telebot
+import pytube
+
+bot_token = '5407530364:AAFKc_kOuxKMv6NaXfOOA5UoLHfrU_v-6GE'
+bot = telebot.TeleBot(bot_token, parse_mode=None)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message,'This is a real fucking bot that fucking converts youtube videos to fucking audio without stupid ads. By Nilu')
+    bot.register_next_step_handler(message, convert)
+    
+
+@bot.message_handler(content_types=['url'], func=lambda message: True)
+def convert(message):
+    url = message.text
+    bot.reply_to(message, 'Got the link.')
+    chat_id = message.chat.id
+    try:
+        yt = pytube.YouTube(url)
+        yt.thumbnail_url
+        stream = yt.streams.filter( file_extension='mp3', only_audio=True).first()
+        stream.download()
+        bot.reply_to(message, 'Downloaded')
+        #send audio to user
+        audio = open(stream.default_filename, 'rb', )
+        #send mp3 to user
+        bot.send_audio(chat_id, audio)
+        bot.reply_to(message, 'Sent, you fucking cunt')
+    except:
+        bot.send_message(chat_id, 'Error')
+    #continue accepting links
+    bot.send_message(chat_id, 'Send me another link')
+    bot.register_next_step_handler(message, convert)
+    
+
+bot.infinity_polling()
